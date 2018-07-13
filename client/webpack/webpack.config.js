5de8f0ac7ceeca71aaa0905afb13b3c5b42f88da
yang-wei <yangweilim21@gmail.com>
2018-07-13 11:40:12 +0900

    update front end package

--- client/webpack/webpack.config.js
+++ client/webpack/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             {
               loader: 'babel-loader',
               query: {
-                presets:['es2015', 'react'],
+                presets:['env', 'react'],
                 cacheDirectory: true
               }
             }
